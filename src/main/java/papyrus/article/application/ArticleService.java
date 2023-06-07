43771e276d7ca9db9ab6c2d1076e5d3a86e71cd4
joyfulviper <kimsk3113@naver.com>
2023-06-08 00:54:50 +0900

    feat: 페이징 기반 페이징 기사 조회 추가

--- src/main/java/papyrus/article/application/ArticleService.java
+++ src/main/java/papyrus/article/application/ArticleService.java
@@ -3,8 +3,12 @@ package papyrus.article.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import papyrus.article.application.dto.ArticleResponse;
+import papyrus.article.domain.Document;
 import papyrus.article.domain.repository.ArticleRepository;
 import papyrus.article.infrastructure.rabbitmq.dto.Message;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,4 +20,12 @@ public class ArticleService {
     public void save(Message message) {
         articleRepository.save(message);
     }
+
+    public List<ArticleResponse> findAllByCursor(Long cursorId, int limit) {
+        return articleRepository.findAllByCursor(cursorId, limit)
+                .stream()
+                .map(ArticleResponse::from)
+                .toList();
+    }
+
 }
--- src/main/java/papyrus/article/domain/Document.java
+++ src/main/java/papyrus/article/domain/Document.java
@@ -2,6 +2,7 @@ package papyrus.article.domain;
 
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -9,6 +10,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "articles")
+@Getter
 public class Document extends Article{
 
     @Id
