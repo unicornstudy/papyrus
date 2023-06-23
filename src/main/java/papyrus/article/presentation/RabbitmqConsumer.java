744cb3acc865686b8b9b06fba5b7739f00283709
doyeon <kimsk3113@naver.com>
2023-06-24 04:21:42 +0900

    docs: 노션 페이지 추가

--- src/main/java/papyrus/article/presentation/RabbitmqConsumer.java
+++ src/main/java/papyrus/article/presentation/RabbitmqConsumer.java
@@ -3,7 +3,6 @@ package papyrus.article.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import papyrus.article.application.ArticleService;
 import papyrus.article.infrastructure.rabbitmq.dto.Message;
 
@@ -13,7 +12,6 @@ public class RabbitmqConsumer {
 
     private final ArticleService articleService;
 
-    @Transactional
     @RabbitListener(queues = "daum")
     public void consumeMessage(Message message) {
         articleService.save(message);
